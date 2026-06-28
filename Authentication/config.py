1d4e4f23aea514e120b15ed2ca359d1768941837
Ziad Mahmoud <ziad.mahmoud.amr@gmail.com>
2026-06-29 00:51:18 +0300

    make postgres fields optional, use DATABASE_URL from Railway

--- Authentication/config.py
+++ Authentication/config.py
@@ -9,9 +9,9 @@ class Settings(BaseSettings):
         extra="ignore",
     )
 
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
+    POSTGRES_USER: str = ""
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
     POSTGRES_HOST: str = "localhost"
     POSTGRES_PORT: int = 5434
 
